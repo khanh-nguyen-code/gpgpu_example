6fdae96afd66bb96f5eb266c62f26e5aaa976f30
khanh nguyen <khanh.nguyen.contact@gmail.com>
2022-10-14 17:45:33 +0800

    m

--- include/util.h
+++ include/util.h
@@ -2,20 +2,15 @@
 #define __UTIL__
 
 #include<string>
-#include<iostream>
+#include<fstream>
+#include<sstream>
 
 namespace util{
-    const int max_read_size = 1024*1024;
     std::string read(const std::string& path) {
-        std::FILE* fp = std::fopen(path.c_str(), "r");
-        if (!fp) {
-            std::cerr << "cannot open file:" << path << std::endl;
-            std::exit(1);
-        }
-        char* content = (char*) std::malloc(max_read_size * sizeof(char));
-        int size = std::fread(content, 1, max_read_size, fp);
-        std::fclose(fp);
-        return std::string(content);
+        std::ifstream t(path);
+        std::stringstream buffer;
+        buffer << t.rdbuf();
+        return buffer.str();
     }
 }
 #endif
