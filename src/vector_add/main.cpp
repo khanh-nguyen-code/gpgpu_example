87e91cf9895dd185eccafa9b675d60d8259e3890
khanh nguyen <khanh.nguyen.contact@gmail.com>
2022-10-23 06:07:16 +0800

    m

--- src/vector_add/main.cpp
+++ src/vector_add/main.cpp
@@ -11,10 +11,10 @@ const int device_id = 0;
 
 cl_int code;
 const int n = 1024;
-const double eps = 1e-6;
+const float eps = 1e-6;
 
-std::vector<double> vector_add(const std::vector<double>& a, const std::vector<double>& b) {
-    std::vector<double> c;
+std::vector<float> vector_add(const std::vector<float>& a, const std::vector<float>& b) {
+    std::vector<float> c;
     for (int i=0;; i++) {
         if (i >= a.size() or i >= b.size()) {
             break;
@@ -23,12 +23,12 @@ std::vector<double> vector_add(const std::vector<double>& a, const std::vector<d
     }
     return c;
 }
-bool vector_cmp(const std::vector<double>& a, const std::vector<double>& b) {
+bool vector_cmp(const std::vector<float>& a, const std::vector<float>& b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i=0; i<a.size(); i++) {
-        double diff = a[i] - b[i];
+        float diff = a[i] - b[i];
         diff = (diff >= 0) ? diff : -diff;
         if (diff > eps) {
             return false;
@@ -39,9 +39,9 @@ bool vector_cmp(const std::vector<double>& a, const std::vector<double>& b) {
 
 int main() {
     util::random_seed(1234);
-    std::vector<double> a = util::random_normal(n);
-    std::vector<double> b = util::random_normal(n);
-    std::vector<double> c(n);
+    std::vector<float> a = util::random_normal(n);
+    std::vector<float> b = util::random_normal(n);
+    std::vector<float> c(n);
 
     cl_device_id device = cl_util::get_device(platform_id, device_id);
     cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &code);
@@ -52,18 +52,18 @@ int main() {
     const std::string source(reinterpret_cast<char*>(&kernel_cl[0]), kernel_cl_len);
     cl_kernel kernel = cl_util::create_kernel(context, source, "vector_add");
 
-    cl_mem a_buf = clCreateBuffer(context, CL_MEM_READ_ONLY, a.size() * sizeof(double), nullptr, &code);
+    cl_mem a_buf = clCreateBuffer(context, CL_MEM_READ_ONLY, a.size() * sizeof(float), nullptr, &code);
     cl_util::assert("create_buffer_with_properties", code);
-    cl_mem b_buf = clCreateBuffer(context, CL_MEM_READ_ONLY, b.size() * sizeof(double), nullptr, &code);
+    cl_mem b_buf = clCreateBuffer(context, CL_MEM_READ_ONLY, b.size() * sizeof(float), nullptr, &code);
     cl_util::assert("create_buffer_with_properties", code);
-    cl_mem c_buf = clCreateBuffer(context, CL_MEM_WRITE_ONLY, c.size() * sizeof(double), nullptr, &code);
+    cl_mem c_buf = clCreateBuffer(context, CL_MEM_WRITE_ONLY, c.size() * sizeof(float), nullptr, &code);
     cl_util::assert("create_buffer_with_properties", code);
     
     // queue
     std::vector<cl_event> write_event(2);
-    code = clEnqueueWriteBuffer(queue, a_buf, CL_NON_BLOCKING, 0, a.size() * sizeof(double), a.data(), 0, nullptr, &write_event[0]);
+    code = clEnqueueWriteBuffer(queue, a_buf, CL_NON_BLOCKING, 0, a.size() * sizeof(float), a.data(), 0, nullptr, &write_event[0]);
     cl_util::assert("enqueue_write_buffer", code);
-    code = clEnqueueWriteBuffer(queue, b_buf, CL_NON_BLOCKING, 0, b.size() * sizeof(double), b.data(), 0, nullptr, &write_event[1]);
+    code = clEnqueueWriteBuffer(queue, b_buf, CL_NON_BLOCKING, 0, b.size() * sizeof(float), b.data(), 0, nullptr, &write_event[1]);
     cl_util::assert("enqueue_write_buffer", code);
 
     cl_event kernel_event;
@@ -89,7 +89,7 @@ int main() {
 
 
     cl_event read_event;
-    code = clEnqueueReadBuffer(queue, c_buf, CL_NON_BLOCKING, 0, c.size() * sizeof(double), c.data(), 1, &kernel_event, &read_event);
+    code = clEnqueueReadBuffer(queue, c_buf, CL_NON_BLOCKING, 0, c.size() * sizeof(float), c.data(), 1, &kernel_event, &read_event);
     cl_util::assert("enqueue_read_buffer", code);
 
     code = clWaitForEvents(1, &read_event);
@@ -102,7 +102,7 @@ int main() {
     clReleaseEvent(kernel_event);
     clReleaseEvent(read_event); 
     // compare
-    std::vector<double> c_host = vector_add(a, b);
+    std::vector<float> c_host = vector_add(a, b);
     if (vector_cmp(c, c_host)) {
         std::printf("result ok\n");
     } else {
