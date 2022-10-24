c4e746a07de76cbe819f3132341ffc9152fa8716
khanh nguyen <khanh.nguyen.contact@gmail.com>
2022-10-24 08:44:21 +0800

    m

--- clblast_bench.cpp
+++ clblast_bench.cpp
@@ -13,9 +13,9 @@ const int device_id = 0;
 
 cl_int code;
 // (m x k) (k x n) -> (m x n)
-const int m = 4096;
-const int n = 4096;
-const int k = 4096;
+const int m = 2048;
+const int n = 2048;
+const int k = 2048;
 
 
 
